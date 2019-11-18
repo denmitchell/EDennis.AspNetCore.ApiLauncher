1b5594226138d3778299033d2d85c0881b7ce29a
Dennis Mitchell <den.mitchell@yahoo.com>
2019-11-18 03:40:08 -0500

    Add code comments to reusable Block method

--- EDennis.Samples.Utils/LauncherUtils.cs
+++ EDennis.Samples.Utils/LauncherUtils.cs
@@ -6,8 +6,10 @@ using System.Threading;
 namespace EDennis.Samples.Utils {
     public static class LauncherUtils {
         public static void Block(string[] args) {
-
+            //parse ewh argument, which carries the name of the synchronization event as a GUID
             Regex pattern = new Regex("(?<=ewh[= ])[A-Za-z0-9\\-]+");
+
+            //if the ewh argument exists, create the EventWaitHandle and block on it
             foreach (var match in args.Where(a => pattern.IsMatch(a)).Select(a => pattern.Match(a))) {
                 var guid = match.Value;
                 using EventWaitHandle ewh = new EventWaitHandle(
@@ -16,6 +18,8 @@ namespace EDennis.Samples.Utils {
                 ewh.WaitOne();
                 return;
             }
+
+            //otherwise, block on ReadKey
             Console.WriteLine($"{ new string('-', 60)}\nRunning until any key is pressed\n{new string('-', 60)}");
             Console.ReadKey();
             return;
